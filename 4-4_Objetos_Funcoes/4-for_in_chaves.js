6e848a94fedcfcf18ef87d899cf6fa55113c4f8f
Ricardo <ricardorosa@gmail.com>
2020-07-16 18:16:10 -0300

    Exercício 4 - Usar um for/in para imprimir no console os valores de cada chave.

---
+++ 4-4_Objetos_Funcoes/4-for_in_chaves.js
@@ -0,0 +1,10 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim",
+};
+
+for (propriedade in info) {
+    console.log(info[propriedade]);
+}
