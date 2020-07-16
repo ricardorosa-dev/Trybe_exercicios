319924586ae2300c661be40e5e9278e6254e8788
Ricardo <ricardorosa@gmail.com>
2020-07-16 18:43:28 -0300

    Exercício 5 - Criar um segundo objeto e printar um texto com informações com os dois. Não entendi se o texto deve ser printado com console.log, ou de alguma forma com o for/in que não entendi.

---
+++ 4-4_Objetos_Funcoes/5-segundo_objeto.js
@@ -0,0 +1,20 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim",
+};
+
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim"
+};
+
+//Tem como acessar os valores de dois objetos em um for/in?
+
+console.log(info.personagem + " e " + info2.personagem);
+console.log(info.origem + ", " + info2.origem);
+console.log(info.nota + " e " + info2.nota);
+console.log("Ambos recorrentes");
