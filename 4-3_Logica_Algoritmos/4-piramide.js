1b96ff6a4358ac2e0b625343c3c1167c4163534f
Ricardo <ricardorosa@gmail.com>
2020-07-15 22:09:37 -0300

    Exercício 4 -Criar uma pirâmide de asteriscos. Fiz criando 'checkers' pra primeira e pra segunda metades da string.

--- 4-3_Logica_Algoritmos/4-piramide.js
+++ 4-3_Logica_Algoritmos/4-piramide.js
@@ -1,13 +1,21 @@
 let n = 5;
-// let check = 3;
-// let linha = "";
+let checkStart = 3; //2
+let checkEnd = 3;
+let linha = "";
 
-// for (let i = 0; i < n; i++) {
-//     if (i !== )
-// }
+console.log("");
+console.log("")
 
-let e = "*****"
-console.log(e);
-
-e.split("*");
-console.log(e);
+for (let j = 0; j < n - 2; j++) {
+    for (let i = 0; i < n; i++) {
+        if (i < checkStart - 1 || i > checkEnd - 1) {
+            linha += " ";
+        } else {
+            linha += "*";
+        }
+    }
+    console.log(linha);
+    linha = "";
+    checkStart -= 1;
+    checkEnd += 1;
+}
