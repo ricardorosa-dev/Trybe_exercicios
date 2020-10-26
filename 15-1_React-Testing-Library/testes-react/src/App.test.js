ea0ed84074902bbd13be65ec56beea928b2e995e
ricardorosa-dev <ricardorosa@gmail.com>
2020-10-26 14:57:31 -0300

    Dia 15. React Testing Library

--- 15-1_React-Testing-Library/testes-react/src/App.test.js
+++ 15-1_React-Testing-Library/testes-react/src/App.test.js
@@ -1,9 +1,41 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Verificando se existe o campo email', () => {
+  const { getByLabelText } = render(<App />);
+  const inputEmail = getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail.type).toBe('email');
+});
+
+test('Verificando se existe um botão de enviar', () => {
+  const { getByTestId } = render(<App />);
+  const btn = getByTestId('id-send');
+  expect(btn).toBeInTheDocument();
+  expect(btn.type).toBe('button');
+  expect(btn).toHaveValue('Enviar');
+});
+
+test('Verificando se existem dois botões', () => {
+  const { getAllByRole } = render(<App />);
+  const buttons = getAllByRole('button');
+  expect(buttons.length).toBe(2);
+});
+
+test('Verifica se o botão e o campo email estão funcionando', () => {
+  const { getByTestId, getByLabelText } = render(<ValidEmail email={ EMAIL_USER } />);
+
+  const EMAIL_USER = 'email@email.com';
+
+  const textEmail = getByTestId('id-email-user');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail).toHaveTextContent('Valor: ');
+
+  const btnSend = getByTestId('id-send');
+  const inputEmail = getByLabelText('Email');
+  fireEvent.change(inputEmail, { target: { value: EMAIL_USER } });
+  fireEvent.click(btnSend);
+  expect(inputEmail).toHaveValue('');
+  expect(textEmail).toHaveTextContent(`Valor: ${EMAIL_USER}`);
 });
---
+++ 16-3_Pratica/teste.js
@@ -0,0 +1,6 @@
+const itemPrimario = [1, 2, 3, 4, 5];
+let clonagemTeste = itemPrimario;
+let clonagemReatribuicao = clonagemTeste.concat(6);
+console.log(clonagemTeste); // [1, 2, 3, 4, 5]
+console.log(itemPrimario); // [1, 2, 3, 4, 5]
+console.log(clonagemReatribuicao);
