5aee6feb9e4ea28378eb05b0e0ef4405c230b309
ricardorosa-dev <ricardorosa@gmail.com>
2021-02-02 20:21:18 -0300

    primeiro exercicio do dia 26.3

---
+++ 26.3_Arquitetura/exercise1.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+function readMyFile(file1, file2) {
+  const fileStartTime = Date.now();
+  
+  const startTime = Date.now();
+  const myFile = fs.readFileSync(file1, 'utf-8'); 
+  const readingTime = Date.now() - startTime;
+  const file1size = fs.statSync(file1).size / (1024*2014);
+  
+  const startTime2 = Date.now();
+  const myFile2 = fs.readFileSync(file2, 'utf-8'); 
+  const readingTime2 = Date.now() - startTime2;
+  const file2size = fs.statSync(file2).size / (1024*1024); // Mb
+  
+  console.log(
+    `File 1: ${readingTime} \nFile 2: ${readingTime2}`
+  );
+  console.log(
+    `File 1 size: ${file1size.toFixed(1)} \nFile 2: ${file2size.toFixed(1)}`
+  );
+  
+  const fileReadingTime = Date.now() - fileStartTime;
+  console.log(`The total reading time for this script is ${fileReadingTime} milliseconds.`);
+}
+
+readMyFile('./gaijin.txt', 'mongo.pdf');
